9a90128a165274377b1ce95b6ceebfd088861ab0
Eray Akgül <90953626+Erayakg@users.noreply.github.com>
2023-11-18 22:03:25 +0300

    first

---
+++ src/main/java/com/example/TarimAraziProject/TarimAraziProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TarimAraziProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarimAraziProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarimAraziProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TarimAraziProject/TarimAraziProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TarimAraziProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarimAraziProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
